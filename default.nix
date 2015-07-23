54b1ec891b18d6db7dd34d6c9337d444036704bf
Alexander Foremny <aforemny@posteo.de>
2015-07-23 16:10:21 +0200

    Add default.nix
    NIX_PATH=nixpkgs=../nixpkgs nix-env -f. -iA env
      installs environment
    
    NIX_PATH=nixpkgs=../nixpkgs nix-build default.nix -A env
      creates a symlink preventing garbage collection

--- default.nix
+++ default.nix
@@ -2,13 +2,11 @@ rec {
 
   pkgs = import <nixpkgs> {};
 
-  ghc = pkgs.haskell.packages.ghc784.ghcWithPackages ( pkgs: with pkgs;
-    [ elm-reactor elm-compiler elm-make elm-package ]
-  );
-
   env = pkgs.buildEnv {
     name = "grol";
-    paths = [ ghc ];
+    paths = with pkgs.haskellPackages; [
+      elm-compiler elm-reactor elm-make elm-package elm-repl
+    ];
   };
 
 }
